2d241f254646790e255fbf33cd36a9d08b92c957
Shreyas Shriyan <51715896+shreyas-shriyan@users.noreply.github.com>
2020-12-13 21:30:25 +0530

    completed logic to add todo items #2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,41 @@
 import React, { useState } from 'react'
-import styles from './home.module.css'
+import styles from './style.module.css'
 import { useDispatch, useSelector } from 'react-redux';
+import { addTodo } from '../redux/actions'
+import PendingItem from './PendingItem'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
     let [input, setInput] = useState("")
 
-    const handleEnter = (e) => {
-        if (e.charCode === 13) {
-            console.log("pressed enter")
+    let dispatch = useDispatch()
 
+    let pending = useSelector((state) => state.pending)
+
+    const handleEnter = (e) => {
+        let payload = {
+            todo: input,
+            created: new Date()
         }
+        if (e.charCode === 13) {
+            dispatch(addTodo(payload))
+            setInput("")
+        }
+
     }
 
     return (
         <div className={styles.flex}>
             <div className={styles.mainCard}>
                 <input value={input} onKeyPress={(e) => handleEnter(e)} onChange={(e) => setInput(e.target.value)} className={styles.input}></input>
+
                 <div className={styles.pending}>
-                    <div className={styles.heading}>Pending</div>
+                    <h3>Pending</h3>
+                    <div>
+                        {pending.sort((a, b) => b.created - a.created).map((item) => <PendingItem key={uuidv4()} data={item} ></PendingItem>)}
+                    </div>
                 </div>
+
             </div>
         </div>
     )
