c094568aa2c58ee302b9373a538946c2a4c1e634
Shreyas Shriyan <51715896+shreyas-shriyan@users.noreply.github.com>
2020-12-13 23:44:33 +0530

    finished adding items to completed section #4

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,12 @@
-import { ADD_TODO } from './actionTypes';
+import { ADD_TODO, COMPLETE_TODO } from './actionTypes';
 
 export const addTodo = (payload) => ({
     type: ADD_TODO,
     payload
 })
+
+export const completeTodo = (payload) => ({
+    type: COMPLETE_TODO,
+    payload
+})
+
